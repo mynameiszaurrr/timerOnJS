c7bc9cd04b4cd9678d97d74745787b5571d61ef7
mynameiszaurrr <77456813+mynameiszaurrr@users.noreply.github.com>
2021-04-25 20:14:05 +0300

    Add files via upload
    fix blinking date and code improvement

--- timer.js
+++ timer.js
@@ -23,28 +23,23 @@ function pushDateOnSite () {
         seconds_block = document.querySelector('#seconds'),
         interval = setInterval(clockRefresh, 1000);
 
+    clockRefresh()
+
+    function zeroChek (numb) {
+        if (numb<10) {
+            return `0${numb}`
+        }
+        else {
+            return numb
+        }
+    }
+
     function clockRefresh() {
         const endTime = timeLeftOnDate();
-        if (endTime.days<10) {
-            day_blocks.textContent=`0${endTime.days}`;
-        } else {
-            day_blocks.textContent= endTime.days;
-        }
-        if (endTime.hours<10) {
-            hours_block.textContent = `0${endTime.hours}`;
-        } else {
-            hours_block.textContent = endTime.hours;
-        }
-        if (endTime.minutes<10) {
-            minutes_block.textContent = `0${endTime.minutes}`;
-        } else {
-            minutes_block.textContent = endTime.minutes;
-        }
-        if (endTime.seconds<10){
-            seconds_block.textContent = `0${endTime.seconds}`;
-        } else {
-            seconds_block.textContent = endTime.seconds;
-        }
+        day_blocks.textContent= zeroChek(endTime.days);
+        hours_block.textContent = zeroChek(endTime.hours);
+        minutes_block.textContent = zeroChek(endTime.minutes);
+        seconds_block.textContent = zeroChek(endTime.seconds);
         if (endTime.msc <= 0) {
             clearInterval(interval)
         }
